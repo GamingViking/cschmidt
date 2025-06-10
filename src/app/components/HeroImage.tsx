ed7d16a151b7c6d1aa5930d22205857f939eae40
Chris Schmidt <cmschmidt12345@gmail.com>
2025-06-10 19:54:06 -0400

    Updated HeroImage alt text for autumn leaves picture

--- src/app/components/HeroImage.tsx
+++ src/app/components/HeroImage.tsx
@@ -5,7 +5,7 @@ import LeafPicture_compressed from "../../../public/images/LeafPicture_compresse
 const HeroImage = () => {
     return(
         <div className="relative w-full h-screen bg-slate-500">
-            <img className="absolute w-full h-full object-cover" src={LeafPicture_compressed.src} alt="vibrant fantasy landscape backdrop" />
+            <img className="absolute w-full h-full object-cover" src={LeafPicture_compressed.src} alt="Cool colored autumn leaves" />
             <div className="absolute flex flex-col justify-center items-center inset-0 text-white">
                 <h1 className="md:text-6xl text-4xl">Chris Schmidt</h1>
                 <p className="md:text-2xl text-base p-4">Welcome to my website!</p>
