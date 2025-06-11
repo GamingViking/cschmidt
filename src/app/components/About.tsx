bc5b0255daa48ef20b89927231931ed087ff38d5
Chris Schmidt <cmschmidt12345@gmail.com>
2025-06-11 18:05:17 -0400

    text updates

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -6,8 +6,7 @@ const About = () => {
         <div className="flex flex-col items-center py-5">
                 <div className="w-5/6 text-lg text-white text-center bg-gradient-to-t from-slate-700 via-blue-950 to-purple-950 bg-fixed rounded-lg border-gray-400 border-2 p-4 transition-transform duration-300 ease-out transform hover:scale-105">
                     <Link href="https://gamingviking.github.io/Resume/index.html" rel="noopener rerefferer" target="_blank">
-                        <p>I am an aspiring full stack developer who has a passion for learning and languages. This inspired me to major in Mandarin Chinese as an undergrad and start to learn coding after graduation. I like to say that I'm still pursuing language, but now I talk to machines in addition to people! I love video games, LARPing, fantasy, gymnastics, field hockey, and so much more! Check below to see some of the stuff I'm into and what I'm working on.</p>
-                        <p className="text-xl pt-2 font-bold">(Click anything with a border to be sent to more info on it!)</p>
+                        <p>I am a full stack developer who has a passion for learning and languages. This inspired me to major in Mandarin Chinese as an undergrad and learn coding after graduation. I like to say that I'm still pursuing language, but now I talk to machines in addition to people! I love video games, LARPing, fantasy, gymnastics, field hockey, and so much more! Check below to see some of the stuff I'm into and what I'm working on.</p>
                     </Link>
                 </div>
         </div>
--- src/app/components/HeroImage.tsx
+++ src/app/components/HeroImage.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from "next/image";
 import LeafPicture_compressed from "../../../public/images/LeafPicture_compressed.png"
 
 const HeroImage = () => {
@@ -8,7 +7,7 @@ const HeroImage = () => {
             <img className="absolute w-full h-full object-cover" src={LeafPicture_compressed.src} alt="Cool colored autumn leaves" />
             <div className="absolute flex flex-col justify-center items-center inset-0 text-white">
                 <h1 className="md:text-6xl text-4xl">Chris Schmidt</h1>
-                <p className="md:text-2xl text-base p-4">Welcome to my website!</p>
+                <p className="md:text-2xl text-base p-4"><span className="font-bold">C</span>reative <span className="font-bold">H</span>onest <span className="font-bold">R</span>eliable <span className="font-bold">I</span>ntelligent <span className="font-bold">S</span>illy</p>
             </div>
         </div>
     )
